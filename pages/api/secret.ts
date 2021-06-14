507dc1c5ab003ca954be97625fef24626576fee9
guillermo <gblasco@bloobirds.com>
2021-06-14 13:11:47 +0200

    fix: error check

--- pages/api/secret.ts
+++ pages/api/secret.ts
@@ -5,53 +5,59 @@ import {GetSecretRequest, GetSecretResponse, SetSecretRequest} from "../../src/t
 
 
 export default async (req: NextApiRequest, res: NextApiResponse<any>) => {
-  if (req.method === 'POST') {
-    const request = req.body as SetSecretRequest;
-    const response = await supabase
-        .from<definitions["Secret"]>("Secret")
-        .insert({
-          KeyId: request.id,
-          Secret: request.secret,
-          Salt: request.salt,
-          AesIv: request.iv,
-          SearchKey: request.searchKey,
-          ExpiresAt: new Date((new Date()).getTime() + 1000 * 60 * 60).toISOString(),
-        });
-    if (!response.error) {
-      res.status(204).json({ });
-    } else {
-      console.log(response.error);
-      res.status(500).json({});
+  try {
+    if (req.method === 'POST') {
+        const request = req.body as SetSecretRequest;
+        const response = await supabase
+            .from<definitions["Secret"]>("Secret")
+            .insert({
+              KeyId: request.id,
+              Secret: request.secret,
+              Salt: request.salt,
+              AesIv: request.iv,
+              SearchKey: request.searchKey,
+              ExpiresAt: new Date((new Date()).getTime() + 1000 * 60 * 60).toISOString(),
+            });
+        if (!response.error) {
+          res.status(204).json({});
+        } else {
+          console.log(response.error);
+          res.status(500).json({});
+        }
+      return;
     }
-    return;
-  }
-  if (req.method === 'DELETE') {
-    const apiRequest = req.body as GetSecretRequest;
-    const dbResponse = await supabase
-        .from<definitions["Secret"]>("Secret")
-        .delete({returning: 'representation', count: null})
-        .eq("SearchKey", apiRequest.searchKey);
-    if (!dbResponse.error) {
-      if (dbResponse.body.length === 1) {
-        const row = dbResponse.body[0];
-        if (row.KeyId === apiRequest.keyId) {
-          const apiResponse:GetSecretResponse = {
-            secret: row.Secret,
-            salt: row.Salt,
-            iv: row.AesIv,
-          };
-          res.status(200).json(apiResponse);
+    if (req.method === 'DELETE') {
+      const apiRequest = req.body as GetSecretRequest;
+      const dbResponse = await supabase
+          .from<definitions["Secret"]>("Secret")
+          .delete({returning: 'representation', count: null})
+          .eq("SearchKey", apiRequest.searchKey);
+      if (!dbResponse.error) {
+        if (dbResponse.body.length === 1) {
+          const row = dbResponse.body[0];
+          if (row.KeyId === apiRequest.keyId) {
+            const apiResponse:GetSecretResponse = {
+              secret: row.Secret,
+              salt: row.Salt,
+              iv: row.AesIv,
+            };
+            res.status(200).json(apiResponse);
+            return;
+          }
+        } else {
+          res.status(404).json({});
           return;
         }
       } else {
-        res.status(404).json({});
+        console.log(dbResponse.error);
+        res.status(500).json({});
         return;
       }
-    } else {
-      console.log(dbResponse.error);
-      res.status(500).json({});
-      return;
     }
+    res.status(404).json({});
+  } catch (e) {
+    console.log(e.message);
+    console.log(e);
+    res.status(500).json({});
   }
-  res.status(404).json({});
 }
