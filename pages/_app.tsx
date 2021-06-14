a8afa90f8fcb824dc676387fe020507607a90563
guillermo <gblasco@bloobirds.com>
2021-06-14 14:44:42 +0200

    feat: added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import '../styles/globals.css'
 import 'antd/dist/antd.css';
+import Head from "next/head";
 
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Head>
+      <title>Transient Secret | Share secrets</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta property="og:type" content="website" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+    </Head>
+    <Component {...pageProps} />
+    </>);
 }
 export default MyApp
