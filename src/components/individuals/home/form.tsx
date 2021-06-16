554a8db56e67cd637aee4660409842985f70f238
Esther <esthermangas95@gmail.com>
2021-06-16 17:35:40 +0200

    added random key un form

--- src/components/individuals/home/form.tsx
+++ src/components/individuals/home/form.tsx
@@ -1,4 +1,6 @@
 import {Button, Form, Input} from "antd";
+import {useEffect, useState } from "react";
+import { uint8ArrayToBase64 } from "../../../utils";
 
 export interface SecretFormData {
     password:string;
@@ -9,41 +11,55 @@ interface HomeFormProps {
 }
 
 export default function HomeForm(props:HomeFormProps) {
-    return (<>
-        <Form
-            layout="vertical"
-            name="basic"
-            initialValues={{ remember: true }}
-            onFinish={props.onSubmit}
-        >
-            <Form.Item
-                label="Secret"
-                name="secret"
-                rules={[{ required: true, message: 'Don\'t miss the secret!' }]}
-            >
-                <Input.TextArea rows={6} />
-            </Form.Item>
+    const [key, setKey] = useState<String>('');
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() => {
+        let array = window.crypto.getRandomValues(new Uint8Array(16));
+        setKey(uint8ArrayToBase64(array));
+        setLoaded(true);
+    }, []);
+    return (
+        <>
+            {loaded && (
+                <>
+                    <Form
+                        layout="vertical"
+                        name="basic"
+                        initialValues={{ remember: true }}
+                        onFinish={props.onSubmit}
+                    >
+                        <Form.Item
+                            label="Secret"
+                            name="secret"
+                            rules={[{ required: true, message: 'Don\'t miss the secret!' }]}
+                        >
+                            <Input.TextArea rows={6} />
+                        </Form.Item>
 
-            <Form.Item
-                label="Key"
-                name="key"
-                rules={[{ required: true, message: 'Please input your key!' }]}
-            >
-                <Input.Password />
-            </Form.Item>
+                        <Form.Item
+                            label="Key"
+                            name="key"
+                            initialValue={key}
+                            rules={[{ required: true, message: 'Please input your key!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
 
-            <Form.Item
-                label="Expires after"
-                name="expires"
-            >
-                <Input value="60 minutes" placeholder="60 minutes" disabled={true} />
-            </Form.Item>
+                        <Form.Item
+                            label="Expires after"
+                            name="expires"
+                        >
+                            <Input value="60 minutes" placeholder="60 minutes" disabled={true} />
+                        </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
-    </>)
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </>
+            )}
+        </>
+        )
 }
